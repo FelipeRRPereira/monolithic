55a5604ac98d57e20660a8c54dca1f8dddaeb61e
Felipe Pereira <felipe.pereira@tectoy.com>
2025-08-12 13:23:29 -0300

    feat: add PaymentFacadeFactory for creating PaymentFacade instances

---
+++ src/modules/payment/factory/facade.factory.ts
@@ -0,0 +1,12 @@
+import PaymentFacade from '../facade/payment.facade'
+import TransactionRepository from '../repository/transaction.repository'
+import ProcessPaymentUseCase from '../usecase/process-payment/process-payment'
+
+export default class PaymentFacadeFactory {
+  static create(): PaymentFacade {
+    const transactionRepository = new TransactionRepository()
+    const processPaymentUseCase = new ProcessPaymentUseCase(transactionRepository)
+
+    return new PaymentFacade(processPaymentUseCase)
+  }
+}
