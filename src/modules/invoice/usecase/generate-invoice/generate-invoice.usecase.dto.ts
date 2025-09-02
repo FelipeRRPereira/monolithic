7ca317c4c91082dd22a1c30bd8083b0e51111366
Felipe Pereira <felipe.pereira@tectoy.com>
2025-09-02 15:12:25 -0300

    feat: create generate invoice usecase dto

---
+++ src/modules/invoice/usecase/generate-invoice/generate-invoice.usecase.dto.ts
@@ -0,0 +1,37 @@
+export interface GenerateInvoiceUseCaseInputDto {
+  id?: string
+  name: string
+  document: string
+  street: string
+  number: string
+  complement: string
+  city: string
+  state: string
+  zipCode: string
+  items: {
+    id?: string
+    name: string
+    price: number
+    quantity: number
+  }[]
+}
+
+export interface GenerateInvoiceUseCaseOutputDto {
+  id: string
+  name: string
+  document: string
+  street: string
+  number: string
+  complement: string
+  city: string
+  state: string
+  zipCode: string
+  items: {
+    id: string
+    name: string
+    price: number
+    quantity: number
+  }[]
+  total: number
+  createdAt: Date
+}
